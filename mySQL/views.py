44a7c607a81059ffa249b72a831ad0bc4e2d0519
Joao Victor <joao.vr.fernandes@gmail.com>
2024-11-02 22:41:28 -0300

    views.py adaptado pra MySQL.connector

---
+++ mySQL/views.py
@@ -0,0 +1,66 @@
+import mysql.connector
+from conexao import criar_conexao, fechar_conexao
+
+def idCheck(con,id):
+    cursor = con.cursor()
+    try:
+        cursor.execute("SELECT COUNT(*) FROM usuario_adm WHERE id_adm = %s", (id,))
+        return cursor.fetchone()[0] > 0
+    except mysql.connector.Error as e:
+        print(f"Error ao verificar id : {e}")
+        return False
+    finally:
+        cursor.close()
+
+def acessar_arquivos_usuario(con, id):
+    cursor = con.cursor()
+    
+    try:
+        # Criando a view, se ela não existir
+        cursor.execute('''
+            CREATE VIEW IF NOT EXISTS view_usuario AS 
+            SELECT nome, tipo, PA 
+            FROM Arquivo
+            JOIN Compartilhamento ON Arquivo.ID_arq = Compartilhamento.ID_arq
+            WHERE Compartilhamento.ID_us = %s
+        ''', (id,))
+        
+        # Consultando a view para obter os dados
+        cursor.execute('SELECT * FROM view_usuario')
+        view_usuario = cursor.fetchall()
+        
+        # Exibindo os resultados
+        for row in view_usuario:
+            print(row)
+            
+    except mysql.connector.Error as err:
+        print(f"Erro ao acessar arquivos do usuário: {err}")
+        
+    finally:
+        cursor.close()
+
+def acessar_arquivos_ADM(con, id):
+    cursor = con.cursor()
+    try:
+        if(idCheck(con, id)):
+
+            cursor.execute('''  
+                CREATE VIEW IF NOT EXISTS  view_adm as
+                SELECT * 
+                FROM Arquivos
+
+
+                ''')
+            
+            view_adm = cursor.execute(''' SELECT * FROM view_adm ''')
+
+            for row in view_adm:
+                print(row)
+        else:
+            print(f"ID {id} não é ADM!!")
+    except mysql.connector.Error as err:
+        print(f"Erro ao acessar arquivos do usuário: {err}")    
+    finally:
+        cursor.close()
+
+            
