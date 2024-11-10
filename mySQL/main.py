f83402a6f222c92f1a37ebd30cf506dcc5b61693
Lucas Ribeiro D'Azevedo <128042824+luconnes@users.noreply.github.com>
2024-11-10 17:22:55 -0300

    Update main.py
    mudancas apos o testes

--- mySQL/main.py
+++ mySQL/main.py
@@ -5,17 +5,16 @@ from CRUD import *
 
 def menu():
     print("Opções: ")
-    print("1 - Fazer Login")
-    print("2 - Inserir usuário")
-    print("3 - Inserir instituição")
-    print("4 - Criar arquivo")
-    print("5 - Fazer comentário")
-    print("6 - Criar plano")
-    print("7 - Compartilhar arquivo")
-    print("8 - Acessar arquivo")
-    print("9 - Criar role e atribuir privilégios")
-    print("10 - Pedir suporte")
-    print("11 - Adicionar arquivo")
+    print("1 - Inserir usuário")
+    print("2 - Inserir instituição")
+    print("3 - Criar arquivo")
+    print("4 - Fazer comentário")
+    print("5 - Criar plano")
+    print("6 - Compartilhar arquivo")
+    print("7 - Acessar arquivo")
+    print("8 - Criar role e atribuir privilégios")
+    print("9 - Pedir suporte")
+    print("10 - Remover arquivo")
     print("0 - Sair")
     
 def main():
@@ -25,7 +24,21 @@ def main():
     global login
     login = None
 
+    login_input = input(("Login: "))
+    senha = input(("Senha: "))
+    #checa se o login existe
+    #checa se a senha bate com o login
+    status = check_login(con, login_input, senha)
+    #
+    if status == True:
+        login = login_input
+        print(f"Login realizado, seja bem vindo(a) {login}\n")
+    else:
+        print("Não foi possivel realizar login, login ou senha invalidos")
+
+
     while op != 0:
+
         menu()
 
         try:
@@ -34,22 +47,7 @@ def main():
             print("Entrada inválida, por favor, insira um número.")
             continue
 
-        if op == 1:#fazer login
-            login_input = input(("Login: "))
-            senha = input(("Senha: "))
-            #checa se o login existe
-            #checa se a senha bate com o login
-            status = check_login(con, login_input, senha)
-            #
-            if status == True:
-                login = login_input
-                print(f"Login realizado, seja bem vindo(a) {login}\n")
-            else:
-                print("Não foi possivel realizar login, login ou senha invalidos")
-
-            #
-        elif op == 2: # insere usuário
-            
+        if op == 1: # insere usuário
             login = input(("Login: "))
             #checa se o login existe
             senha = input(("Senha: "))
@@ -61,50 +59,52 @@ def main():
             resposta = int(input(("É administrador?\n1 - sim\n2- não\n")))
             if resposta == 1:
                 inserir_adm(con, login)
-        elif op == 3: # insere instituição
+            print("Usuário adicionado com sucesso!\n")
+        elif op == 2: # insere instituição
             nome = input(("Nome: "))
             endereco = input(("Endereço: "))
             causa_social = input(("Causa social: "))
             insere_instituicao(con, nome, endereco, causa_social)
-        elif op == 4: # cria arquivo
-            print("a fazer")
-        elif op == 5: # fazer comentário
+            print("Instituição adicionada com sucesso!\n")
+        elif op == 3: # cria arquivo
+            nome = input("Nome do arquivo: ")
+            tipo = input("Tipo (exemplo: .exe): ")
+            permissao_acesso = input("Permissão de acesso (público/privado): ")
+            id_usuario = input("Id do usuário: ")
+            url = input("URL: ")
+            adicionar_arquivo(con, nome, tipo, permissao_acesso, id_usuario, url)
+        elif op == 4: # fazer comentário
             id_arquivo = input(("Id do arquivo: "))
             conteudo = input(("Conteúdo do comentário: "))
-            fazerComentario(con, id_arquivo, conteudo, login=login)
-        elif op == 6: # criar plano
+            fazerComentario(con, id_arquivo, conteudo, login)
+        elif op == 5: # criar plano
             nome = input(("Nome: "))
             duracao = input(("Duração (HH:MM:SS): "))
             data_aquisicao = input(("Data de aquisição (AAAA-MM-DD): "))
             espaco_usuario = input(("Espaço do usuário: "))
             insere_plano(con, nome, duracao, data_aquisicao, espaco_usuario)
-        elif op == 7: # compartilhar arquivo
+        elif op == 6: # compartilhar arquivo
             id_arquivo = input("Id do arquivo: ")
             id_dono = input(("Id do dono: "))
             id_compartilhado = input(("Id do compartilhado: "))
-            data = input(("Data (AAAA-MM-DD): "))
-            compartilhar(con, id_arquivo, id_dono, id_compartilhado, data)
-        elif op == 8: # acessar arquivo específico
+            compartilhar(con, id_arquivo, id_dono, id_compartilhado)
+        elif op == 7: # acessar arquivo específico
             nome_arquivo = input(("Nome do arquivo: "))
             acessar_arquivo(con, nome_arquivo, login=login)
-        elif op == 9: # criar role
+        elif op == 8: # criar role
             nomeRole = input(("Que role você deseja criar? "))
             criarRole(con, nomeRole)
             privilegios = []
             privilegios = input(("Que privilégio deseja dar? [...,...] (escreva em caixa alta e separando por virgulas): "))
             privilegios = [priv.strip() for priv in input.split(',')]
             concederPrivilegios(con, nomeRole, privilegios)
-        elif op == 10: # pedir suporte
+        elif op == 9: # pedir suporte
             id_arquivo = input(("Sobre que arquivo você deseja pedir o supórte (id)?" ))
             mensagem = input(("Descrição do suporte: "))
             pedir_suporte(con, id_arquivo, mensagem, login=login)
-        elif op == 11:  #adiciona arquivo
-            nome = input("Nome do arquivo: ")
-            tipo = input("Tipo do arquivo: ")
-            permissao_acesso = input("Permissão de acesso (Ex.: 'r' para leitura, 'rw' para leitura e escrita): ")
-            id_usuario = int(input("ID do usuário: "))
-            url = input("URL do arquivo: ")
-            adicionar_arquivo(con, nome, tipo, permissao_acesso, id_usuario, url)
+        elif op == 10:  # remover arquivo
+            id_arquivo = input("Id do arquivo a ser deletado: ")
+            remover_arquivo(con, id_arquivo)
     
     print("Saindo do programa...")
     fechar_conexao(con)
