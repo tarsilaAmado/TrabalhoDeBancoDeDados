ec590f8dbc3e2306544a0cc5425dbd56fca9ef5b
Júlia Saldanha <juliasvilar@gmail.com>
2024-11-17 17:21:49 -0300

    try except

--- mySQL/conexao.py
+++ mySQL/conexao.py
@@ -1,9 +1,14 @@
 # fazendo a conexão com o banco de dados previamente criado em SQL
 
 import mysql.connector
+from mysql.connector import Error
 
 def criar_conexao (host, usuario, senha, banco):
-    return mysql.connector.connect(host = host, user = usuario, password = senha, database = banco)
+    try:
+        return mysql.connector.connect(host = host, user = usuario, password = senha, database = banco)
+    except mysql.connector.Error as e:
+        print(f"Erro ao atribuir role : {e}")
+        return None
 
 def fechar_conexao (con):
     return con.close()
